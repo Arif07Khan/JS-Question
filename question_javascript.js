93fcf9722e5a5b04bfc7705360c60856ab53be1f
Arif07Khan <arifkhanstar1997@gmail.com>
2023-07-26 12:11:57 +0530

    first commit

---
+++ question_javascript.js
@@ -0,0 +1,17 @@
+// Write a program to check whether a number is even or odd.
+
+// Write a program to calculate the sum of two numbers.
+
+// Write a program to print all the elememts is a array.
+
+// Write a program to calculate the factorial of a given number using a recursive function.
+
+// Write a program to find the largest element in an array of numbers.
+
+// Write a program to check if a given string is a palindrome or not.
+
+// Write a program to count the number of vowels in a given string.
+
+// Write a program to convert a temperature from Celsius to Fahrenheit and vice versa.
+
+// Write a program to find the sum of all elements in an array.
